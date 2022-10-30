cfc00e0f8ded895bfef3762800920e45d631360a
Ivan Edrennikov <i.edrennikov@corp.badoo.com>
2022-10-30 23:30:27 +0200

    Add logo and text

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Logo_BAS_InLine.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Botanical Art Shop</h1>
-        under construction
-      </header>
+        <img src={logo} className="App-logo" alt="logo" /><br />
+We are working to make the online shop here.<br />
+<br />
+But you can see and buy now our gifts on<br />
+<a href="https://thebotanicalartshop.etsy.com/">https://thebotanicalartshop.etsy.com/</a><br />
+<br />
+Thank you!<br />
+
+<h4>Contact</h4>
+
+Tallinn, Estonia, 10114<br />
+Mail: <a class="App-link" href="mailto://artist@botanicalartshop.com">artist@botanicalartshop.com</a><br />
+<br />
+© 2022. Botanical Art Shop.
     </div>
   );
 }
